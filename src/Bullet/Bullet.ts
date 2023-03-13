57b3e3907de7cfad22a50655b842ae47d0027b25
ifhito <hito01010101@gmail.com>
2023-03-13 22:48:31 +0900

    feat: 具体的なロジックを継承先に移動

--- src/Bullet/Bullet.ts
+++ src/Bullet/Bullet.ts
@@ -1,6 +1,6 @@
 interface Bullet {
-  width: number;
-  height: number;
+  x: number;
+  y: number;
   ballRadius: number;
   fillStyle: string;
   speed: number;
@@ -10,10 +10,10 @@ interface Bullet {
 }
 import GameElement from "../GameElement";
 class Bullet extends GameElement implements Bullet {
-  constructor(width, height, ballRadius, fillStyle, speed, damage, canvas) {
+  constructor(x, y, ballRadius, fillStyle, speed, damage, canvas) {
     super(Math.random());
-    this.width = width;
-    this.height = height;
+    this.x = x;
+    this.y = y;
     this.ballRadius = ballRadius;
     this.fillStyle = fillStyle;
     this.speed = speed;
@@ -22,34 +22,11 @@ class Bullet extends GameElement implements Bullet {
     this.ctx = canvas.getContext("2d");
   }
   // 作成
-  create() {
-    this.ctx.beginPath();
-    this.ctx.arc(this.width, this.height, this.ballRadius, 0, Math.PI * 2);
-    console.log(this.width);
-    this.ctx.fillStyle = this.fillStyle;
-    this.ctx.fill();
-    this.ctx.closePath();
-  }
+  create() {}
   // 動かす
-  move() {
-    this.create();
-    this.width += 1;
-    this.height -= 1;
-  }
-  outOfRange() {
-    if (this.canvas.width - this.ballRadius < this.width) return true;
-    if (this.width < -this.ballRadius) return true;
-    if (this.canvas.height - this.ballRadius < this.height) return true;
-    if (this.height < -this.ballRadius) return true;
-    return false;
-  }
-  // 消す
-  remove() {
-    if (this.outOfRange()) return true;
-    return false;
-  }
+  move() {}
   // 当たる
-  hit(ctx, canvas) {}
+  hit() {}
 }
 
 export default Bullet;
