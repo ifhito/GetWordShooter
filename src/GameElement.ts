254ba3ca5c4325c72f5eb5826d59a8bfdda1402e
ifhito <hito01010101@gmail.com>
2023-03-13 22:47:41 +0900

    fix: removeとoutOfRangeを移動

--- src/GameElement.ts
+++ src/GameElement.ts
@@ -6,5 +6,13 @@ class GameElement implements GameElement {
   constructor(hash) {
     this.hash = hash;
   }
+  outOfRange() {
+    return true;
+  }
+  // 消す
+  remove() {
+    if (this.outOfRange()) return true;
+    return false;
+  }
 }
 export default GameElement;
